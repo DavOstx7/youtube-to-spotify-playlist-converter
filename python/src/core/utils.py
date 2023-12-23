d3a729ede01ac5586f8d07413204c67cbc4b70e4
dav ost <private>
2023-12-23 22:03:48 +0200

    python: add async support for older versions

--- python/src/core/utils.py
+++ python/src/core/utils.py
@@ -1,9 +1,25 @@
+import asyncio
+import sys
 import os
 import json
 import logging.config
+from typing import TypeVar, Awaitable
 
 _FILE_DIR = os.path.dirname(__file__)
 _CONFIG_DIR = os.path.join(_FILE_DIR, "../../../config/")
+
+T = TypeVar("T")
+
+
+def run_async(main: Awaitable[T]) -> T:
+    if sys.version_info >= (3, 7):
+        return asyncio.run(main)
+    else:
+        loop = asyncio.get_event_loop()
+        try:
+            return loop.run_until_complete(main)
+        finally:
+            loop.close()
 
 
 def load_config_file(filename: str):
